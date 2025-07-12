995f9c5cb42c3983533346f1d3ac4570d5768643
Brian Goff <cpuguy83@gmail.com>
2025-07-12 15:01:00 -0700

    Remove a bunch of commented or unused options

--- configuration.nix
+++ configuration.nix
@@ -16,28 +16,13 @@
 
 
   # Bootloader.
-  # Lanzaboote currently replaces the systemd-boot module.
-  # This setting is usually set to true in configuration.nix
-  # generated at installation time. So we force it to false
-  # for now.
-  # boot.loader.systemd-boot.enable = false;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
   networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Enable networking
   networking.networkmanager.enable = true;
-  # networking.bridges = {
-  #   br0 = {
-  #     interfaces = [ "enp8s0" ];
-  #   };
-  # };
 
   # Set your time zone.
   time.timeZone = "America/Los_Angeles";
@@ -62,7 +47,6 @@
   services.xserver.enable = true;
   services.xserver.videoDrivers = [ "amdgpu" ];
 
-
   # Configure keymap in X11
   services.xserver.xkb = {
     layout = "us";
@@ -80,22 +64,13 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.cpuguy83 = {
     isNormalUser = true;
     description = "Brian Goff";
-    extraGroups = [ "networkmanager" "wheel" "docker" "libvirtd" "kvm" ];
+    extraGroups = [ "networkmanager" "wheel" "docker" "kvm" ];
     packages = with pkgs; [
       ghostty
       stow
@@ -120,9 +95,7 @@
     ];
   };
 
-  users.groups.libvirtd.members = ["cpuguy83"];
   users.groups.docker.members = ["cpuguy83"];
-
 
   programs.firefox.enable = true;
 
@@ -137,11 +110,6 @@
           containerd-snapshotter = true;
         };
       };
-    };
-
-    libvirtd = {
-      enable = true;
-      allowedBridges = [ "birbr0" "br0" ];
     };
 
     spiceUSBRedirection = {
@@ -179,16 +147,15 @@
     ++
 
     (with pkgs-unstable; [
-      pkgs-unstable.firefox
-      pkgs-unstable.qemu_kvm
-      pkgs-unstable.qemu_full
-      pkgs-unstable.virglrenderer
-      pkgs-unstable.mesa
-      pkgs-unstable.libglvnd
-      pkgs-unstable.libGL
-      pkgs-unstable.virt-manager
-      pkgs-unstable.libvirt
-      pkgs-unstable.vscode
+      firefox
+      qemu_kvm
+      qemu_full
+      virglrenderer
+      mesa
+      libglvnd
+      libGL
+      virt-manager
+      vscode
       (pkgs.vscode.override { isInsiders = true; })
     ]);
 
@@ -198,25 +165,6 @@
 
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
   programs.virt-manager.enable = true;
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ]-;
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
