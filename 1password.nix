8ad2e8e0d4d4966f807044b8adaae050f1552bb5
Brian Goff <cpuguy83@gmail.com>
2025-07-14 15:38:12 -0700

    Make sure 1pass.nix has allowUnfree set in it

--- 1password.nix
+++ 1password.nix
@@ -1,6 +1,11 @@
-{ ... }:
+{ lib, ... }:
 
 {
+  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+    "1password-gui"
+    "1password"
+  ];
+
   programs._1password.enable = true;
   programs._1password-gui = {
     enable = true;
