30cea5608b7ad0428858d20c7f71cd08e9c8c463
Brian Goff <cpuguy83@gmail.com>
2025-07-16 13:33:37 -0700

    Make intune actually work... sort of...

--- msft-corp/default.nix
+++ msft-corp/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, config, lib, ... }:
+{ pkgs, pkgs-unstable, config, lib, ... }:
 
 with lib; let
   cfg = config.msft-corp;
@@ -9,12 +9,26 @@ in {
       default = false;
     };
   };
+
   config = mkIf cfg.enable {
+    # Make sure that the unstable channel is used for these packages
+    # I think this is necessary because `services.intune.enable` is referencing
+    # the stable channel otherwise.
+    # 
+    # Unstable is needed to pick up a few fixes that are not currently in stable
+    # such that stable is non-functional.
+    nixpkgs.overlays = lib.mkAfter [
+      (final: prev: {
+        microsoft-identity-broker = pkgs-unstable.microsoft-identity-broker;
+        intune-portal = pkgs-unstable.intune-portal;
+      })
+    ];
+
     services.intune.enable = true;
     environment.systemPackages = [
       pkgs.microsoft-edge
-      pkgs.intune-portal
-      pkgs.microsoft-identity-broker
+      pkgs-unstable.intune-portal
+      pkgs-unstable.microsoft-identity-broker
     ];
   };
 }
