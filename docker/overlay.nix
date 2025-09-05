e5a79b60f7ae2a6c124d31dfcde938c85d757f7d
Brian Goff <cpuguy83@gmail.com>
2025-09-05 16:10:47 -0700

    Update docker to 28.4 and buildx to 0.28

--- docker/overlay.nix
+++ docker/overlay.nix
@@ -1,21 +1,21 @@
 (self: prev: {
   docker_28 = prev.docker_28.override {
-    version = "28.3.3";
-    cliRev = "v28.3.3";
-    cliHash = "sha256-LsV9roOPw0LccvBUeF3bY014OwG6QpnVsLf+dqKyvsg=";
-    mobyRev = "v28.3.3";
-    mobyHash = "sha256-YfdnCAc9NgLTuvxLHGhTPdWqXz9VSVsQsfzLD3YER3g=";
+    version = "28.4.0";
+    cliRev = "v28.4.0";
+    cliHash = "sha256-SgePAc+GvjZgymu7VA2whwIFEYAfMVUz9G0ppxeOi7M=";
+    mobyRev = "v28.4.0";
+    mobyHash = "sha256-hiuwdemnjhi/622xGcevG4rTC7C+DyUijE585a9APSM=";
     containerd = prev.containerd;
   };
 
   docker-buildx = prev.docker-buildx.overrideAttrs (_: rec {
-    version = "0.27.0";
+    version = "0.28.0";
     rev = "1";
     src = prev.fetchFromGitHub {
         owner = "docker";
         repo = "buildx";
         rev = "v${version}";
-        sha256 = "sha256-DdG2z0raDHcbBMDl7C0WORKhG0ZB9Gvie8u4/isE8ow=";
+        sha256 = "sha256-sYhmXVc1pU0nzG57AIuaLqUOWz9MfFlpJZQ9B5Ki5ik=";
       };
       ldflags = [
         "-w"
