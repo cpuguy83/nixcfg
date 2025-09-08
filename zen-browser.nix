46e471a722ad84cf0b479c0274668d6afba45e3b
Brian Goff <cpuguy83@gmail.com>
2025-09-08 14:40:27 -0700

    zen: add nixpkgs search

--- zen-browser.nix
+++ zen-browser.nix
@@ -27,6 +27,52 @@ in {
             pwas-for-firefox
             onepassword
           ];
+          search.force = true;
+          search.engines = {
+            nix-packages = {
+              name = "Nix Packages";
+              urls = [
+                {
+                  template = "https://search.nixos.org/packages";
+                  params = [
+                    { name = "channel"; value = "unstable"; }
+                    { name = "query";   value = "{searchTerms}"; }
+                  ];
+                }
+              ];
+              icon           = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+              definedAliases = [ "@np" ];
+            };
+
+            nix-options = {
+              name = "Nix Options";
+              urls = [
+                {
+                  template = "https://search.nixos.org/options";
+                  params = [
+                    { name = "channel"; value = "unstable"; }
+                    { name = "query";   value = "{searchTerms}"; }
+                  ];
+                }
+              ];
+              icon           = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+              definedAliases = [ "@no" ];
+            };
+
+            nixos-wiki = {
+              name = "NixOS Wiki";
+              urls = [
+                {
+                  template = "https://wiki.nixos.org/w/index.php";
+                  params = [
+                    { name = "search"; value = "{searchTerms}"; }
+                  ];
+                }
+              ];
+              icon           = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+              definedAliases = [ "@nw" ];
+            };
+          };
         };
       };
 
