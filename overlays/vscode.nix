a0cb898c761fe41e689df34f3c233c2c9d822d48
Brian Goff <cpuguy83@gmail.com>
2025-09-10 11:44:37 -0700

    Update vscode version

--- overlays/vscode.nix
+++ overlays/vscode.nix
@@ -3,7 +3,7 @@ self: super: {
   vscode = self.vscodeInsidersBase.overrideAttrs (old: {
     src = (builtins.fetchTarball {
       url = "https://code.visualstudio.com/sha/download?build=insider&os=linux-x64";
-      sha256 = "1rqc5w0xss4kmf7viiqg3yskv4450an0divmw5mvifzqsvnik6js";
+      sha256 = "1317h66y5syw2bjsvmb7gcalikp0va88c65zarl419vbzi3vfy2i";
     });
     version = "latest";
   });
