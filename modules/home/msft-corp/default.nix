0006d5e7028e7fa277eb5ac6119243f5a01ff333
Brian Goff <cpuguy83@gmail.com>
2025-12-13 18:07:52 -0800

    Add microsoft-edge-stable bin pointing to microsoft-edge

--- modules/home/msft-corp/default.nix
+++ modules/home/msft-corp/default.nix
@@ -15,7 +15,11 @@ in
     };
 
     home.packages = [
-      pkgs-unstable."microsoft-edge"
+      pkgs-unstable.microsoft-edge
+      # Teams PWA still references the old microsoft-edge-stable binary name
+      (pkgs.writeShellScriptBin "microsoft-edge-stable" ''
+        exec ${pkgs-unstable.microsoft-edge}/bin/microsoft-edge "$@"
+      '')
     ];
   };
 }
