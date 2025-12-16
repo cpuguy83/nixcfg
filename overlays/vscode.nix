8769807dc95b002455cc6eaa762bdb014e2864c7
Brian Goff <cpuguy83@gmail.com>
2025-12-16 11:52:37 -0800

    system update

--- overlays/vscode.nix
+++ overlays/vscode.nix
@@ -4,7 +4,7 @@ self: super: {
     src = (
       builtins.fetchTarball {
         url = "https://code.visualstudio.com/sha/download?build=insider&os=linux-x64";
-        sha256 = "0vmajw1i6mjhrvy30pkvwil5paszc4wzbrb90fmxi5zxxarxxc3c";
+        sha256 = "0ajy3zqrc8fcixk5zk7xc4w066dqxz36fhrdkdpr80h05zqb4cpj";
       }
     );
     version = "latest";
