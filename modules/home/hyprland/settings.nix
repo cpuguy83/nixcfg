a10e5bb9921009e9ae76b0b748dc5064743bc9b5
Brian Goff <cpuguy83@gmail.com>
2026-01-21 15:45:19 -0800

    Do not make windows slightly transparent when inactive

--- modules/home/hyprland/settings.nix
+++ modules/home/hyprland/settings.nix
@@ -120,7 +120,7 @@ in
         rounding = 10;
         rounding_power = 2;
         active_opacity = 1.0;
-        inactive_opacity = 0.95;
+        inactive_opacity = 1.0;
 
         shadow = {
           enabled = true;
