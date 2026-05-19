1d7c80966989c251024912603a8f3361671cf5d5
Brian Goff <cpuguy83@gmail.com>
2026-05-19 10:04:24 -0700

    adjust monitor alignment to shift mouse deadzones
    Since switching the left monitor to use a vertical orientation, this
    makes the mouse cursor crossover a little more natural.

--- hosts/yavin4/home.nix
+++ hosts/yavin4/home.nix
@@ -6,8 +6,8 @@
   ];
 
   mine.desktop.hyprland.monitors = [
-    "DP-1,preferred,auto-right,auto"
-    "DP-2,preferred,auto-left,auto,transform,3"
+    "DP-1,preferred,2160x840,1"
+    "DP-2,preferred,0x0,1,transform,3"
 
     # These are used as backchannels to DP-1 and DP-2 for DDC/CI
     "HDMI-A-1,disable"
